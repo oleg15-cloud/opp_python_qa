f5235441db55c1df9a455228661f749eb7ed273b
oleg15-cloud <karaivanoleg093@gmail.com>
2021-07-11 13:43:14 +0300

    improvement of validation

--- src/triangle.py
+++ src/triangle.py
@@ -5,29 +5,21 @@ from math import sqrt
 class Triangle(Figure):
 
     def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.name = "Triangle"
-
-    def is_it_triangle(self):
-        if self.a + self.b > self.c and self.a + self.c > self.b and self.b + self.c > self.a:
-            return True
+        if a + b > c and a + c > b and b + c > a:
+            self.a = a
+            self.b = b
+            self.c = c
+            self.name = "Triangle"
         else:
-            return None
+            raise AttributeError("This triangle is impossible")
 
     @property
     def perimeter(self):
-        if self.is_it_triangle() is True:
-            return round(self.a + self.b + self.c, 2)
-        else:
-            return None
+        return round(self.a + self.b + self.c, 2)
+
 
     @property
     def area(self):
-        if self.is_it_triangle() is True:
-            part_of_perimeter = (self.a + self.b + self.c) / 2
-            return round(sqrt(part_of_perimeter * (part_of_perimeter - self.a) * (part_of_perimeter - self.b) * (
-                    part_of_perimeter - self.c)), 2)
-        else:
-            return None
+        part_of_perimeter = (self.a + self.b + self.c) / 2
+        return round(sqrt(part_of_perimeter * (part_of_perimeter - self.a) * (part_of_perimeter - self.b) * (
+                part_of_perimeter - self.c)), 2)
